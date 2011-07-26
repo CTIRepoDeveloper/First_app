fb632bc2b12588971c45617f1cb435156ed2a593
CTI Repo Developer <lemo@provide.net>
2011-07-26 07:07:46 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e2381055231ec80bd393f1a3e323f8935316279ca44d28a4bfa3c71a033a2193f85f1a81105d827b71c0d6ca4bda30a69ce217f531b3347dde703e4736bcd487'
